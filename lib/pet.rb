49fbf2c81241577efbf1a8509fa2d07dc1063780
Phil DeLong <philjdelong@gmail.com>
2019-10-19 14:45:07 -0600

    Complete iteration 1

--- lib/pet.rb
+++ lib/pet.rb
@@ -1,18 +1,19 @@
 class Pet
-  attr_reader :name,
-              :type
+  attr_reader :name, :type
+  attr_accessor :fed
 
-  def initialize(attributes)
-    @name = attributes[:name]
-    @type = attributes[:type]
+  def initialize(pet_info)
+    @name = pet_info[:name]
+    @type = pet_info[:type]
     @fed = false
+  end
+
+  def fed?
+    @fed
   end
 
   def feed
     @fed = true
   end
 
-  def fed?
-    @fed
-  end
 end
--- test/pet_test.rb
+++ test/pet_test.rb
@@ -1,27 +1,32 @@
-require 'minitest/autorun'
-require 'minitest/pride'
-require './lib/pet'
+require'minitest/autorun'
+require'minitest/pride'
+require'pry'
+require'./lib/pet'
 
 class PetTest < Minitest::Test
+
+  def setup
+    @samson = Pet.new({name: "Samson", type: :dog})
+  end
+
   def test_it_exists
-    samson = Pet.new({name: "Samson", type: :dog})
-    assert_instance_of Pet, samson
+    # skip
+    assert_instance_of Pet, @samson
   end
 
-  def test_it_has_attributes
-    samson = Pet.new({name: "Samson", type: :dog})
-    assert_equal "Samson", samson.name
-    assert_equal :dog, samson.type
+  def test_it_has_a_name
+    assert_equal "Samson", @samson.name
   end
 
-  def test_it_is_not_fed_by_default
-    samson = Pet.new({name: "Samson", type: :dog})
-    assert_equal false, samson.fed?
+  def test_it_has_a_type
+    assert_equal :dog, @samson.type
   end
 
   def test_it_can_be_fed
-    samson = Pet.new({name: "Samson", type: :dog})
-    samson.feed
-    assert_equal true, samson.fed?
+    assert_equal false, @samson.fed?
+    @samson.feed
+    assert_equal true, @samson.fed?
   end
+
+
 end
